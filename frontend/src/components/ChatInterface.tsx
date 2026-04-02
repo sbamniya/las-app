c189ba00849668f46411893e0f20c5ddd9839b8d
Sonu <sbamniya23@gmail.com>
2026-04-02 23:29:41 +0530

    fix: update regex for list item wrapping in MarkdownRenderer

--- frontend/src/components/ChatInterface.tsx
+++ frontend/src/components/ChatInterface.tsx
@@ -18,7 +18,7 @@ function MarkdownRenderer({ content }: { content: string }) {
     .replace(/\*(.*?)\*/g, '<em>$1</em>')
     .replace(/^- (.*$)/gm, '<li>$1</li>')
     .replace(/^(\d+)\. (.*$)/gm, '<li>$2</li>')
-    .replace(/(<li>.*<\/li>)/s, '<ul>$1</ul>')
+    .replace(/(<li>[\s\S]*<\/li>)/, '<ul>$1</ul>')
     .replace(/\n\n/g, '</p><p>')
     .replace(/\n/g, '<br/>')
     .replace(/\|(.*)\|/g, (match) => {
